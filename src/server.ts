baf3a2ab1dfdca1e9e8b005005129ce7c1c43b64
Ridwan Kolawole <kolawoleri@accessbankplc.com>
2022-03-18 22:55:36 +0100

    documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express'
 import bodyParser from 'body-parser'
 import feeRoute from '@src/routes/feeRoute'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
 
 const port = process.env.PORT || 5000
 const app: Application = express()
 
 app.use(bodyParser.json())
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use("/", feeRoute)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
